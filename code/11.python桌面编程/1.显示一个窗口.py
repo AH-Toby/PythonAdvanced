4387dace4cda67c6b55c2cdf7f5dc9c807ba3a9e
Toby <1751001928@qq.com>
2022-07-21 18:24:22 +0800

    pyqt5内容更新

--- code/11.python桌面编程/1.显示一个窗口.py
+++ code/11.python桌面编程/1.显示一个窗口.py
@@ -6,7 +6,17 @@
 @file: 1.显示一个窗口.py
 @time: 2022/5/26 13:42
 """
+import sys
+from PyQt5.Qt import *
 
-from PyQt5 import *
+app = QApplication(sys.argv)
+
+window = QWidget()
+
+window.setWindowTitle("")
+window.resize(500, 500)
 
 
+window.show()
+
+sys.exit(app.exec_())
---
+++ code/11.python桌面编程/2.窗口展示模板-面向流程.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Toby
+@software: PyCharm
+@file: 2.窗口展示模板-面向流程.py
+@time: 2022/7/21 17:19
+"""
