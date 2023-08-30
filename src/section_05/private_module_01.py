66ac25e44532e4a41af9919c9f9b0488ac624ff0
wangtao <wangtao@yunzhangfang.com>
2023-08-31 00:48:25 +0800

    重新翻译组织Python代码的技巧，优化内部措辞

---
+++ src/section_05/private_module_01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Project ：PythonAdvanced 
+@File    ：private_module_01.py
+@Author  ：Toby
+@Date    ：2022/12/12 17:41 
+@Description：私有化属性和方法的调用
+"""
